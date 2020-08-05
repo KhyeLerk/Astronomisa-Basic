f3f11214569756dce096e6736496cea7065f09f0
KhyeLerk <Khye.19@ichat.sp.edu.sg>
2020-08-05 21:57:43 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DataViewer/precache-manifest.3193dae3d524ee5f1d8c6b0e0012fa2e.js"
+  "/Astronomisa-Basic/precache-manifest.67eb9c4c7c3b9da4d90ec947bbf1b822.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DataViewer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Astronomisa-Basic/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
